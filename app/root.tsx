9d155fb138c094da88c500887af26950a6d014bb
23432875 <122618874+23432875@users.noreply.github.com>
2024-11-07 16:28:33 +0000

    on root.tsx i have added buttons and changed colors

--- app/root.tsx
+++ app/root.tsx
@@ -17,7 +17,20 @@ export default function App() {
 			<div className="flex h-screen flex-col justify-between">
 				<div className="flex-1">
 					<main className="grid h-full place-items-center">
-						<h1 className="text-mega">Welcome to Epic News!</h1>
+						<h1 className="text-mega">Welcome to Anima Lazer!</h1>
+						<p className='text - base text-red-500'>
+							our goal is to make everyone happy!
+						</p>
+						<button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"> 
+							Welcome
+						</button>
+						<button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"> 
+							Welcome
+						</button>
+						<button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"> 
+							Welcome
+						</button>
+
 					</main>
 				</div>
 			</div>
