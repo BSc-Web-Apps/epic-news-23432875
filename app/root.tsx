e1f6a960991cce68e229dc57ec45d43ac405f22c
23432875 <122618874+23432875@users.noreply.github.com>
2024-11-21 15:13:40 +0000

    finished adding links and adding contents to the main roots page

--- app/root.tsx
+++ app/root.tsx
@@ -22,14 +22,41 @@ export default function App() {
 				<div className="flex-1 bg-yellow-300">
 					<main className="grid h-full place-items-center">
 						<h1 className="text-mega text-black">Welcome to Epic News!</h1>
-						<p className="text - base h-8 text-red-500">
-							our goal is to make everyone happy!
-						</p>
+
+						<div className="container mx-auto grid grid-cols-4 gap-14">
+							<div className="flex h-60 w-80 flex-col justify-between bg-amber-100 p-4">
+								<h3 className="text-3xl font-bold text-gray-800">
+									Main Article
+								</h3>
+								<p className="text-xs text-gray-400">Category</p>
+								<button className="rounded bg-orange-500 px-4 py-2 font-bold text-white hover:bg-orange-700">
+									Full Article
+								</button>
+							</div>
+							<div className="flex h-60 w-80 flex-col justify-between bg-amber-100 p-4">
+								<h3 className="text-3xl font-bold text-gray-800">
+									Main Article
+								</h3>
+								<p className="text-xs text-gray-400">Category</p>
+								<button className="rounded bg-orange-500 px-4 py-2 font-bold text-white hover:bg-orange-700">
+									Full Article
+								</button>
+							</div>
+							<div className="flex h-60 w-80 flex-col justify-between bg-amber-100 p-4">
+								<h3 className="text-3xl font-bold text-gray-800">
+									Main Article
+								</h3>
+								<p className="text-xs text-gray-400">Category</p>
+								<button className="rounded bg-orange-500 px-4 py-2 font-bold text-white hover:bg-orange-700">
+									Full Article
+								</button>
+							</div>
+						</div>
 
 						<div className="container mx-auto grid grid-cols-4 gap-8">
 							<div className="flex h-48 w-48 flex-col justify-between bg-amber-100 p-4">
 								<h3 className="text-3xl font-bold text-gray-800">
-									News headline
+									News Article 1
 								</h3>
 								<p className="text-xs text-gray-400">Category</p>
 								<button className="rounded bg-orange-500 px-4 py-2 font-bold text-white hover:bg-orange-700">
@@ -38,7 +65,7 @@ export default function App() {
 							</div>
 							<div className="flex h-48 w-48 flex-col justify-between bg-amber-100 p-4">
 								<h3 className="text-3xl font-bold text-gray-800">
-									News headline
+									News Article 2
 								</h3>
 								<p className="text-xs text-gray-400">Category</p>
 								<button className="rounded bg-orange-500 px-4 py-2 font-bold text-white hover:bg-orange-700">
@@ -47,7 +74,7 @@ export default function App() {
 							</div>
 							<div className="flex h-48 w-48 flex-col justify-between bg-amber-100 p-4">
 								<h3 className="text-3xl font-bold text-gray-800">
-									News headline
+									News Article 3
 								</h3>
 								<p className="text-xs text-gray-400">Category</p>
 								<button className="rounded bg-orange-500 px-4 py-2 font-bold text-white hover:bg-orange-700">
@@ -56,7 +83,46 @@ export default function App() {
 							</div>
 							<div className="flex h-48 w-48 flex-col justify-between bg-amber-100 p-4">
 								<h3 className="text-3xl font-bold text-gray-800">
-									News headline
+									News Article 4
+								</h3>
+								<p className="text-xs text-gray-400">Category</p>
+								<button className="rounded bg-orange-500 px-4 py-2 font-bold text-white hover:bg-orange-700">
+									Full Article
+								</button>
+							</div>
+						</div>
+
+						<div className="container mx-auto grid grid-cols-4 gap-8">
+							<div className="flex h-48 w-48 flex-col justify-between bg-amber-100 p-4">
+								<h3 className="text-3xl font-bold text-gray-800">
+									News Article 5
+								</h3>
+								<p className="text-xs text-gray-400">Category</p>
+								<button className="rounded bg-orange-500 px-4 py-2 font-bold text-white hover:bg-orange-700">
+									Full Article
+								</button>
+							</div>
+							<div className="flex h-48 w-48 flex-col justify-between bg-amber-100 p-4">
+								<h3 className="text-3xl font-bold text-gray-800">
+									News Article 6
+								</h3>
+								<p className="text-xs text-gray-400">Category</p>
+								<button className="rounded bg-orange-500 px-4 py-2 font-bold text-white hover:bg-orange-700">
+									Full Article
+								</button>
+							</div>
+							<div className="flex h-48 w-48 flex-col justify-between bg-amber-100 p-4">
+								<h3 className="text-3xl font-bold text-gray-800">
+									News Article 7
+								</h3>
+								<p className="text-xs text-gray-400">Category</p>
+								<button className="rounded bg-orange-500 px-4 py-2 font-bold text-white hover:bg-orange-700">
+									Full Article
+								</button>
+							</div>
+							<div className="flex h-48 w-48 flex-col justify-between bg-amber-100 p-4">
+								<h3 className="text-3xl font-bold text-gray-800">
+									News Article 8
 								</h3>
 								<p className="text-xs text-gray-400">Category</p>
 								<button className="rounded bg-orange-500 px-4 py-2 font-bold text-white hover:bg-orange-700">
